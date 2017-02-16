91c4d016cfebb1c09d9cfc5f2431ea5bd98758ea
HAN HWI <ming037@naver.com>
2017-02-16 22:56:01 +0900

    Set ActionBar change when lists are clicked

--- app/src/main/java/hh/com/smellslikebakin/ViewPagerFragment.java
+++ app/src/main/java/hh/com/smellslikebakin/ViewPagerFragment.java
@@ -6,7 +6,6 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 /**
  * Created by Administrator on 2017-02-16.
@@ -21,9 +20,13 @@ public class ViewPagerFragment extends Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
         int index = getArguments().getInt(KEY_RECIPE_INDEX);
-
-        Toast.makeText(getActivity(), Recipes.names[index], Toast.LENGTH_SHORT).show();
-        View view = inflater.inflate(R.layout.fragment_viewpager,container,false);
+        getActivity().setTitle(Recipes.names[index]);
+        View view = inflater.inflate(R.layout.fragment_viewpager, container,false);
         return view;
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+        getActivity().setTitle(getResources().getString(R.string.app_name));
     }
 }
