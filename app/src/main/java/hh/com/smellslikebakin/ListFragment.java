c196eacfafd33df92f1f2a3c6f07a9c5bada9cd9
HAN HWI <ming037@naver.com>
2017-02-16 12:52:26 +0900

    Fix double fragment

--- app/src/main/java/hh/com/smellslikebakin/ListFragment.java
+++ app/src/main/java/hh/com/smellslikebakin/ListFragment.java
@@ -1,10 +1,10 @@
 package hh.com.smellslikebakin;
 
+import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,7 +13,7 @@ import android.view.ViewGroup;
  * Created by Administrator on 2017-02-16.
  */
 
-public class ListFragment extends LoggingFragment {
+public class ListFragment extends Fragment {
 
     public interface OnRecipeSelectedInterface {
         void onListRecipeSelected(int index); //MainActivity에서 구현
@@ -22,7 +22,6 @@ public class ListFragment extends LoggingFragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        Log.d(LoggingFragment.TAG, "onCreateView");
 
         OnRecipeSelectedInterface listener = (OnRecipeSelectedInterface) getActivity();
         View view = inflater.inflate(R.layout.fragment_list, container,false);
